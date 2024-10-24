18d4bfb904fe283e1140ae764b716371fe5d9824
Ezra Haugabrooks <ezra@aymscores.com>
2024-10-24 12:08:22 -0400

    Initial commit for Progressions project

---
+++ Progressions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Progressions
+//
+//  Created by E. Haugabrooks on 10/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Progressions/ProgressionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressionsApp.swift
+//  Progressions
+//
+//  Created by E. Haugabrooks on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgressionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
